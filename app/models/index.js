7a054aaf323525d86ef3a9a69a20da5184ced7df
Diego <jdrivaschirre@hotmail.com>
2024-11-14 12:31:34 -0500

    test con postgre

--- app/models/index.js
+++ app/models/index.js
@@ -2,12 +2,16 @@ const { Sequelize } = require("sequelize");
 
 // Passing parameters separately
 const sequelize = new Sequelize(  
-  process.env.MYSQL_DB, 
-  process.env.MYSQL_USER, 
-  process.env.MYSQL_PASSWORD, 
+  process.env.DB_DB, 
+  process.env.DB_USER, 
+  process.env.DB_PASSWORD, 
   {
-  host: process.env.MYSQL_HOST,
-  dialect:'mysql',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect:'postgres',
+  dialectOptions: {
+    ssl: process.env.DB_SSL == "true"
+  }
 });
 
 module.exports = sequelize;
