f5d71e3c90f4f6bc493717e89cd702fb2ff48cbd
Diego <jdrivaschirre@hotmail.com>
2024-11-15 18:49:31 -0500

    Agrego auth middleware

---
+++ app/middlewares/authJwt.middleware.js
@@ -0,0 +1,45 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/user.model');
+const Role = require('../models/role.model');
+
+// Verificar que se ha logeado (se tiene un token)
+exports.verifyToken = (req, res, next) => {
+    let token = req.session.token;
+
+    if (!token) {
+        return res.status(401).send({ message: "No token provided" })
+    };
+
+    jwt.verify(process.env.SECRET_KEY_JWT, 
+        (err, decoded) => {
+            if (err) {
+                return res.status(403).send({ message: "Unauthorized!" })
+            };
+            req.userId = decoded.id;
+            next();
+      });
+};
+
+// Verificar que es admin
+exports.isAdmin = async (req, res, next) => {
+    if (!req.userId) {
+        return res.status(500).send({ message: "Login not detected" })
+    };
+
+    // Encontrar usuario
+    const usuario =  await User.findOne({ where: {id: req.userId} });
+
+    if (!usuario) {
+        return res.status(500).send({ message: "Failed to authenticate user" });
+    };
+    // Determinar si es admin
+    const currentRole = await Role.findOne({ where: {id: usuario.RoleId } });
+    const adminRole = await Role.findOne({ where: {role: "admin"} });
+
+    if (currentRole.id === adminRole.id) {
+        next();
+        return;
+    };
+
+    return res.status(403).send({ message: "Requires Admin Role" });
+};
--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -2,11 +2,13 @@ module.exports = app => {
     let router = require("express").Router();
     const controlador = require('../controllers/auth.controller');
 
+    // User related routes
     router.get("/Users", controlador.findAllUsers);
     router.post("/Registro", controlador.crearUsuario);
     router.post("/Login", controlador.login);
     router.post("/Logout", controlador.logout);
     router.get("/Roles", controlador.findAllRoles);
+    // Product related routes
     router.get("/Home", controlador.listarProductos);
     router.post("/Producto", controlador.crearProducto);
     // router.get("/:name", users.findOne);
